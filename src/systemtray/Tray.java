4c69bd9b854c4a0302659d97e72269521d6aa195
Giorgio Momigliano <gmomigliano@gmail.com>
2016-09-03 00:54:46 +0200

    obsolete version was uploaded - corrected

--- src/systemtray/Tray.java
+++ src/systemtray/Tray.java
@@ -1,12 +1,14 @@
-package main;
+package systemtray;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 
 public class Tray {
 
 	private final TrayIcon trayIcon;
 	private int width, height;
 	private final PopupMenu menu;
+	private final SystemTray tray = SystemTray.getSystemTray();
 
 	public Tray(Image image, int width, int height) {
 
@@ -17,39 +19,49 @@ public class Tray {
 
 			menu = new PopupMenu();
 			trayIcon = new TrayIcon(correctImage(image, width, height), "TrayIcon", menu);
-			
+
 		} else {
-			
+
 			menu = null;
 			trayIcon = null;
 			System.exit(1);
 		}
 	}
-	
-	public void addItemsToTray(MenuItem...items) {
-		
+
+	public void addItems(MenuItem...items) {
+
 		for(MenuItem item : items)
-			
+
 			menu.add(item);
 	}
-	
-	public void addSeparatorToTray() {
-		
+
+	public void addSeparator() {
+
 		menu.addSeparator();
 	}
 	
-	public boolean pushTray() {
+	public void remove() {
 		
+		tray.remove(trayIcon);
+	}
+
+	public void addMouseListener(MouseAdapter e) {
+
+		trayIcon.addMouseListener(e);
+	}
+
+	public boolean pushTray() {
+
 		try {
 
-			SystemTray.getSystemTray().add(trayIcon);
+			tray.add(trayIcon);
 
 		} catch (AWTException e) {
 
 			e.printStackTrace();
 			return false;
 		}
-		
+
 		return true;
 	}
 
@@ -58,13 +70,8 @@ public class Tray {
 		trayIcon.setImage(correctImage(image, width, height));
 	}
 
-	private static Image correctImage(Image image, int width, int height) {
+	private static Image correctImage(Image image, int width, int height) { // Set image to preferred size with correct hints
 
 		return image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-	}
-	
-	public PopupMenu getMenu() {
-		
-		return menu;
 	}
 }
